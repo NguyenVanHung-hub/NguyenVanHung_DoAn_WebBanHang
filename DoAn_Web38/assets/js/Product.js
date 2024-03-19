a7a29d2e6560be73212e8367da4045b9debcd91e
NguyenVanHung-hub <115867880+NguyenVanHung-hub@users.noreply.github.com>
2024-03-19 19:22:07 +0700

    sua

--- DoAn_Web38/assets/js/Product.js
+++ DoAn_Web38/assets/js/Product.js
@@ -1,148 +1,148 @@
 const data = [
     {
         "id" : 1,
-        "name" : "Com Rang Cai Cam",
-        "price" : "25.000",
+        "name" : "Cơm Rang Cải Bò",
+        "price" : "25000 vnd",
         "category":"com rang",
         "image" :"https://images.foody.vn/res/g100008/1000072631/s400x400/8f1bfff1-9f88-4505-a309-11666acb-747467e0-231215133921.jpeg"
     },
     {
         "id" : 2,
-        "name" : "Com Rang Hai San",
-        "price" : "40.000",
+        "name" : "Cơm Rang Hải Sản",
+        "price" : "40000 vnd",
         "category":"com rang",
         "image" :"https://images.foody.vn/res/g119/1180780/s400x400/3d921668-0a2a-460f-8ac0-02ff1002-21d043cd-230906143726.jpeg"
     },
     {
         "id" : 3,
-        "name" : "Com Rang Rua Bo",
-        "price" : "30.000",
+        "name" : "Cơm Rang Cải Bò",
+        "price" : "30000 vnd",
         "category":"com rang",
         "image" :"https://images.foody.vn/res/g100008/1000072631/s400x400/8ed4aab8-c355-421c-b2ef-75184153-4ad459cf-231215133539.jpeg"
     },
     {
         "id" : 4,
-        "name" : "Com Rang Suon Sot",
-        "price" : "30.000",
+        "name" : "Cơm Rang Sườn Sốt",
+        "price" : "30000 vnd",
         "category":"com rang",
         "image" :"https://images.foody.vn/res/g100008/1000072631/s400x400/52735894-40a8-4de5-8824-defab69d-434685b5-231215141849.jpeg"
     },
     {
         "id" : 5,
-        "name" : "Com Rang Dui Ga",
-        "price" : "35.000",
+        "name" : "Cơm Rang Đùi Gà",
+        "price" : "35000 vnd",
         "category":"com rang",
         "image" :"https://images.foody.vn/res/g100008/1000072631/s400x400/9da3fc6c-e5c0-4cd7-a4df-277e699e-cb5e391f-231215134122.jpeg"
     },
     {
         "id" : 6,
-        "name" : "Pho Tai",
-        "price" : "30.000",
+        "name" : "Phở Tái",
+        "price" : "30000 vnd",
         "category":"pho",
         "image" :"https://images.foody.vn/res/g100008/1000070824/s400x400/18303c48-6083-462b-9fe2-d500b9a7-583c206b-231011123745.jpeg"
     },
     {
         "id" : 7,
-        "name" : "Pho Gau",
-        "price" : "40.000",
+        "name" : "Phở Gầu",
+        "price" : "40000 vnd",
         "category":"pho",
         "image" :"https://images.foody.vn/res/g100008/1000070824/s400x400/d5b73434-1fe2-4a91-9532-89a713b2-742d7e82-231011124107.jpeg"
     },
     {
         "id" : 8,
-        "name" : "Pho Tai Nam",
-        "price" : "50.000",
+        "name" : "Phở Tái Năm",
+        "price" : "50000 vnd",
         "category":"pho",
         "image" :"https://images.foody.vn/res/g100008/1000070824/s400x400/dd7de87d-b60c-42c9-96a8-aaf8ce6b-ab094f1d-231020012927.jpeg"
     },
     {
         "id" : 8,
-        "name" : "Pho Ga Ta",
-        "price" : "35.000",
+        "name" : "Phở Gà Ta",
+        "price" : "35000 vnd",
         "category":"pho",
         "image" :"https://images.foody.vn/res/g100008/1000070824/s400x400/fdbbfeec-cc55-43c8-a2dc-c96a2b96-09ec7dc4-231011124619.jpeg"
     },
     {
         "id" : 9,
-        "name" : "My Kim Chi Bo",
-        "price" : "30.000",
+        "name" : "Mỳ Kim Chi Bò",
+        "price" : "30000 vnd",
         "category":"my",
         "image" :"https://images.foody.vn/res/g25/242707/s400x400/d2e8d239-fb9f-40b7-b4b8-d186597c-4862b11a-220413173408.jpeg"
     },
     {
         "id" : 10,
-        "name" : "My Kim Chi Suon Sun",
-        "price" : "50.000",
+        "name" : "Mỳ Kim Chi Sườn Sụn",
+        "price" : "50000 vnd",
         "category":"my",
         "image" :"https://images.foody.vn/res/g25/242707/s400x400/5197b1a3-6386-422c-85bf-4c6b7104-b18da70f-220413183804.jpeg"
     },
     {
         "id" : 11,
-        "name" : "My Kim Chi Dui Ga",
-        "price" : "40.000",
+        "name" : "Mỳ Kim Chi Đùi Gà",
+        "price" : "40000 vnd",
         "category":"my",
         "image" :"https://images.foody.vn/res/g25/242707/s400x400/41698fe7-5fa2-4545-9ed7-4f4c5ab2-74a0f998-220413172758.jpeg"
     },
     {
         "id" : 12,
-        "name" : "My Kim Chi Nam Bo Trung",
-        "price" : "60.000",
+        "name" : "Mỳ Kim Chi Nặm Bò",
+        "price" : "60000 vnd",
         "category":"my",
         "image" :"https://images.foody.vn/res/g25/242707/s400x400/ccbeeb42-71e2-402b-bf2e-8ad9b83e-1120853a-220413173529.jpeg"
     },
     {
         "id" : 13,
-        "name" : "My Kim Chi Hai San",
-        "price" : "55.000",
+        "name" : "Mỳ Kim Chi Hải Sản",
+        "price" : "55000 vnd",
         "category":"my",
         "image" :"https://images.foody.vn/res/g25/242707/s400x400/f53d8ac5-49a6-4ed9-8099-297bc219-5a512683-220413183940.jpeg"
     },
     {
         "id" : 14,
-        "name" : "Bun Bo Hue",
-        "price" : "55.000",
+        "name" : "Bún Bò Huế",
+        "price" : "55000 vnd",
         "category":"bun",
         "image" :"https://images.foody.vn/res/g104/1037686/s400x400/04847146-f0c8-4247-9994-33a5151f-493673ef-201010143235.jpeg"
     },
     {
         "id" : 15,
-        "name" : "Bun Vit",
-        "price" : "40.000",
+        "name" : "Bún Vịt",
+        "price" : "40000 vnd",
         "category":"bun",
         "image" :"https://images.foody.vn/res/g100007/1000061431/s400x400/1a018551-7e9a-47b6-b671-13a73776-2f3032c5-221221093903.jpeg"
     },
     {
         "id" : 16,
-        "name" : "Bun Thit Nuong",
-        "price" : "55.000",
+        "name" : "Bún Thịt Nướng",
+        "price" : "55000 vnd",
         "category":"bun",
         "image" :"https://images.foody.vn/res/g2/12124/s400x400/2017113111948-hmn.jpg"
     },
     {
         "id" : 17,
-        "name" : "Bun Tron",
-        "price" : "50.000",
+        "name" : "Bún Trộn",
+        "price" : "50000 vnd",
         "category":"bun",
         "image" :"https://images.foody.vn/res/g109/1081123/s400x400/3297fa15-64cc-46fa-9bd9-01e3247a-2fa8e767-220309212324.jpeg"
     },
     {
         "id" : 18,
-        "name" : "Banh My Heo Quay",
-        "price" : "40.000",
+        "name" : "Bánh Mỳ Heo Quay",
+        "price" : "40000 vnd",
         "category":"banh my",
         "image" :"https://images.foody.vn/res/g75/749370/s400x400/55c4c97a-89d3-43aa-98de-8ea52e37-78b7e165-210408132613.jpeg"
     },
     {
         "id" : 19,
-        "name" : "Banh My Trung",
-        "price" : "30.000",
+        "name" : "Bánh Mỳ Trứng",
+        "price" : "30000 vnd",
         "category":"banh my",
         "image" :"https://images.foody.vn/res/g75/749370/s400x400/a036cb2e-ea27-41e5-ad59-6a4ff080-e68a45cf-220905221026.jpeg"
     },
     {
         "id" : 20,
-        "name" : "Banh My Cha",
-        "price" : "35.000",
+        "name" : "Bánh Mỳ Chả",
+        "price" : "35000 vnd",
         "category":"banh my",
         "image" :"https://images.foody.vn/res/g75/749370/s400x400/5ba2c1a4-95bb-48fa-9f38-01065d7c-477a1774-220326150401.jpeg"
     },
@@ -170,10 +170,12 @@ const displayData = (data, page) =>{
                <img src=${item.image} alt="" class="img">
                <div class="product-suggest-item-bottom product-item-bottom">
                <span class="name">${item.name}</span>
-               <span class="price">${item.price}</span>
+               
                </div>
            <div class="button-shop">
-           <button>Shop Now</button>
+           <span class="price">${item.price}</span>
+           <button><i class="fa-solid fa-cart-shopping"></i>
+           Mua</button>
            </div> 
            </div>
            
--- DoAn_Web38/assets/js/shop.js
+++ DoAn_Web38/assets/js/shop.js
@@ -4,8 +4,25 @@ const cartModalOverlay = document.querySelector('.cart-modal-overlay');
 const addToCart = document.querySelectorAll('.button-shop');
 const cart = document.querySelector('.cart-btn');
 const close = document.querySelector('#close-btn');
+const gio = document.querySelector('.gio');
 
 
+const getCartLocal = () =>{
+  const cartItems = localStorage.getItem('cartItems');
+ 
+  return cartItems ? JSON.parse(cartItems) : [];
+};
+
+const saveLocal = (cartItems) =>{
+  localStorage.setItem("cartItems" , JSON.stringify(cartItems));
+};
+
+let cartItems = getCartLocal();
+
+
+gio.addEventListener('click', () => {
+  cartModalOverlay.style.transform = "translateX(0)";
+});
 cart.addEventListener('click', () => {
     cartModalOverlay.style.transform = "translateX(0)";
   });
@@ -30,39 +47,55 @@ const addToCartclick = (item) =>{
     let price = cartItem.querySelector('.price').innerHTML;
     let imageSrc = cartItem.querySelector('.img').src;
     let name = cartItem.querySelector('.name').innerHTML;
-    addItemToCart(name, imageSrc, price);
-    cartModalOverlay.style.transform = "translateX(0)";
-};
-
-const addItemToCart = (name, imageSrc, price) =>{
-    const productRows = document.querySelector('.product-rows');
-    let productRow = document.createElement('div');
-    productRow.classList.add('product-row');
-    const cartImge = document.querySelectorAll('.cart-image');
     let isAdd = false;
-    cartImge.forEach((val) => {
-      if (val.src == imageSrc) {
+    cartItems.forEach((val) => {
+      if (val.imageSrc === imageSrc && val.price === price) {
         alert('San pham da co trong gio hang');
         isAdd = true;
         return;
       }
     })
-    if (isAdd) {
-      return
+    if (isAdd == false) {
+      cartItems.push({
+        price: price,
+        imageSrc: imageSrc,
+        name: name,
+        quantity: 1
+      });
     }
+    
+    saveLocal(cartItems);
+    
+
+    addItemToCart(cartItems);
+    cartModalOverlay.style.transform = "translateX(0)";
+};
+
+const addItemToCart = (cartItems) =>{
+    const productRows = document.querySelector('.product-rows');
+    productRows.innerHTML= ` `;
+  if(cartItems.length >0){
+    
+    cartItems.forEach((item) =>{
+      let productRow = document.createElement('div');
+    productRow.classList.add('product-row');
+    const cartImge = document.querySelectorAll('.cart-image');
+    
     productRow.innerHTML = `
       <div class="pro-shop">
-        <img class="cart-image" src="${imageSrc}" alt="">
+        <img class="cart-image" src="${item.imageSrc}" alt="">
         <div class="pro-shop-bot">
-            <span class ="cart-name">${name}</span>
-            <span class ="cart-price">${price}</span>
+            <span class ="cart-name">${item.name}</span>
+            <span class ="cart-price">${item.price}</span>
         </div>
       </div>
-      <input class="product-quantity" type="number" value="1">
+      <input class="product-quantity" type="number" value="${item.quantity}">
       <button class="remove-btn">Remove</button>
     `;
-
     productRows.appendChild(productRow);
+    })
+  };
+    
 
     removeProduct();
     chanProductValue();
@@ -70,10 +103,15 @@ const addItemToCart = (name, imageSrc, price) =>{
 };
 
 const removeProduct = () =>{
+
     let btnRemove = document.querySelectorAll(".remove-btn");
     btnRemove.forEach((item) =>{
+      let imageSrc = item.parentElement.querySelector('.cart-image').src;
+      
         item.addEventListener("click", () =>{
             item.parentElement.remove();
+            cartItems = cartItems.filter((val) => val.imageSrc !== imageSrc);
+            saveLocal(cartItems);
             updatePrice();
         });
     });
@@ -81,26 +119,33 @@ const removeProduct = () =>{
 
 const chanProductValue = () =>{
     const inputQuality = document.querySelectorAll('.product-quantity');
-    inputQuality.forEach((item) =>{
+    inputQuality.forEach((item,index) =>{
         item.addEventListener("change", () =>{
+            cartItems[index].quantity = item.value;
+            saveLocal(cartItems);
             updatePrice();
         })
     })
 }
 
 const updatePrice = () =>{
-    const productRowEle = document.querySelectorAll('.product-row');
+    // const productRowEle = document.querySelectorAll('.product-row');
     let total = 0;
-    productRowEle.forEach((item) =>{
-        const priceEl = item.querySelector('.cart-price');
-        const price = parseFloat(priceEl.innerHTML.replace('vnd', ' '))
-        const quantity = item.querySelector('.product-quantity').value;
+    cartItems.forEach((item) =>{
+        // const priceEl = item.querySelector('.cart-price');
+        const price = parseFloat(item.price.replace('vnd', ' '))
+        
        
-        total = total + (price * quantity);
+        total = total + (price * item.quantity);
         
 
         document.querySelector('.total-price').innerHTML = total;
-        document.querySelector('.cart-quantity').innerHTML = productRowEle.length;
+        document.querySelector('.cart-quantity').innerHTML = cartItems.length;
     });
 }
 
+
+window.addEventListener('DOMContentLoaded', ()=>{
+  const cartItems =getCartLocal();
+  addItemToCart(cartItems);
+});
